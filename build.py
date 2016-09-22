452c6b32eedd22a9897cb41045cf08c3095ad8fc
wsunccake <wsunccake@hotmail.com>
2016-09-23 07:07:04 +0800

    update build.py setting

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
-# use_plugin("python.coverage")
+use_plugin("python.coverage")
 use_plugin("python.distutils")
 use_plugin('python.pycharm')
 
@@ -17,3 +17,5 @@ default_task = ['clean', 'publish']
 def initialize(project):
     project.version = '0.1'
     project.depends_on('docopt')
+    project.set_property('coverage_threshold_warn', 80)
+    project.set_property('coverage_break_build', False)
