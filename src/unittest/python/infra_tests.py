fe5acb3b0ab849a5f857bd1978e1e70db9ee7ebe
wsunccake <wsunccake@hotmail.com>
2016-10-31 16:02:49 +0800

    add unittest

--- src/unittest/python/infra_tests.py
+++ src/unittest/python/infra_tests.py
@@ -19,11 +19,29 @@ class InfraTest(unittest.TestCase):
         v2.set_basis(0, 0, 1)
         self.assertEqual(v3, v2)
 
+        v4 = Vector(1, 2, 3)
+        self.assertEquals('1.000000, 2.000000, 3.000000', str(v4))
+        self.assertEquals(Vector(4, 4, 4), v4 + Vector(3, 2, 1))
+        self.assertEquals(Vector(0, 0, 0), v4 - Vector(1, 2, 3))
+        self.assertEquals(Vector(5, 10, 15), v4 * 5)
+        self.assertEquals(True, v4 == Vector(1, 2, 3))
+
+        v5 = Vector(10, 0, 0)
+        self.assertEquals(v5, v5.rotate(Vector(1, 0, 0), 360.0))
+
     def test_lattice_init(self):
         l = Lattice(1, 2, 3)
         self.assertEqual([1, 2, 3], l.vectors)
         l.constant = 2
         self.assertEqual(2, l.constant)
+
+        l1 = Lattice(4, 5, 6, 10)
+        self.assertEquals([4, 5, 6], l1.vectors)
+        self.assertEquals(10, l1.constant)
+        l1.set_vectors(2, 2, 2)
+        self.assertEquals([2, 2, 2], l1.vectors)
+        l1.constant = 5
+        self.assertEquals(5, l1.constant)
 
     def test_element(self):
         x = Element()
@@ -32,10 +50,38 @@ class InfraTest(unittest.TestCase):
         self.assertEqual(0, x.atomic_number)
         self.assertEqual(0.0, x.atomic_mass)
 
+        x.symbol = 'Se'
+        x.name = 'Selenium'
+        x.atomic_number = 34
+        x.atomic_mass = 78.971
+        self.assertEqual('Se', x.symbol)
+        self.assertEqual('Selenium', x.name)
+        self.assertEqual(34, x.atomic_number)
+        self.assertEqual(78.971, x.atomic_mass)
+        self.assertEquals('Se, Selenium, 34, 78', str(x))
+
+        y = Element('H', 'Hydrogen', 1, 1)
+        y.copy_element(x)
+        self.assertEquals('Se, Selenium, 34, 78', str(y))
+
     def test_check_element_by_periodic_table(self):
         h = Element('H')
         check_element_by_periodic_table(h)
         self.assertEqual('Hydrogen', h.name)
+        self.assertEquals(1, h.atomic_number)
+        self.assertEquals(1.00794, h.atomic_mass)
+
+        he = Element(name='Helium')
+        check_element_by_periodic_table(he, 'name')
+        self.assertEquals('He', he.symbol)
+        self.assertEquals(2, he.atomic_number)
+        self.assertEquals(4.002602, he.atomic_mass)
+
+        li = Element(number=3)
+        check_element_by_periodic_table(li, 'number')
+        self.assertEquals('Li', li.symbol)
+        self.assertEquals('Lithium', li.name)
+        self.assertEquals(6.941, li.atomic_mass)
 
     def test_atom(self):
         a1 = Atom('H', 1, 2, 3)
@@ -56,4 +102,27 @@ class InfraTest(unittest.TestCase):
         a2 = Atom()
         a2.copy_atom(a1)
 
+        default = Atom()
+        self.assertEquals('X', default.symbol)
+        self.assertEquals((0, 0, 0), default.coordinate)
+        self.assertEquals(('T', 'T', 'T'), default.dynamic)
+        self.assertEquals((0, 0, 0), default.displace)
+
+        default.symbol = 'C'
+        default.set_coordinate(3.52, 4.67, 2.33)
+        default.set_dynamic('F', 'T', 'F')
+        default.set_displace(10.5223, 20.9889, 56.2837)
+        self.assertEquals('C', default.symbol)
+        self.assertEquals((3.52, 4.67, 2.33), default.coordinate)
+        self.assertEquals(('F', 'T', 'F'), default.dynamic)
+        self.assertEquals((10.5223, 20.9889, 56.2837), default.displace)
+        self.assertEquals("('C', 3.52, 4.67, 2.33)", repr(default))
+
+        copy_from_default = Atom()
+        copy_from_default.copy_atom(default)
+        self.assertEquals(True, copy_from_default == default)
+        self.assertEquals(Atom('C', 9.0, 9.0, 9.0), copy_from_default.add_coordinate(5.48, 4.33, 6.67))
+        self.assertEquals(Atom('C', 3.0, 4.0, 2.0), copy_from_default.sub_coordinate(0.52, 0.67, 0.33))
+        self.assertEquals(Atom('C', 10.56, 14.01, 6.99), copy_from_default.mul_coordinate(3))
+        self.assertEquals(Atom('C', 1.76, 2.335, 1.165), copy_from_default.div_coordinate(2))
 
